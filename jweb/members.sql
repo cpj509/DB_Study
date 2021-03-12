ac79c9c89c3b143905b37dd812edc6b7e78284e5
CPJ <abzi110@naver.com>
2021-03-12 15:25:27 +0900

    Create table for homepage board.

--- jweb/members.sql
+++ jweb/members.sql
@@ -8,7 +8,29 @@ create table t_member(
     primary key(memberId)
 );
 
+--t_boadrd 게시판 테이블 생성
+create table t_board(
+    bnum number(4),
+    title varchar2(100) not null,
+    content varchar2(2000) not null,
+    regDate date default sysdate,
+    memberId varchar2(20),
+    primary key(bnum),
+    constraint FK_MemberBoard foreign key(memberId)
+    references t_member(memberId)
+);
+
+--시퀀스 생성 : 게시판 번호(bnum)
+create sequence bSeq
+increment by 1
+start with 1
+minvalue 1
+maxvalue 1000
+nocycle
+nocache;
+
 select * from t_member;
+select * from t_board;
 
 rollback;
 
