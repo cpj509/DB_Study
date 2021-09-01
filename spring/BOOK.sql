5121a7cce9f7a9276d0d9cb3dd8787025f8fd8a4
CPJ <abzi110@naver.com>
2021-09-01 23:42:14 +0900

    part3 학습용 DB 작성.

---
+++ spring/BOOK.sql
@@ -0,0 +1,16 @@
+-- 시퀀스 생성시에 일반적으로 seq_로 시작함.
+create SEQUENCE seq_board;
+
+-- 테이블 생성시에 일반적으로 tbl_나 t_로 시작함.
+create table tbl_board(
+    bno number(10, 0),
+    title VARCHAR2(200) not null,
+    content varchar2(2000) not null,
+    writer varchar2(50) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate
+);
+
+-- bno를 PK로 지정.
+alter table tbl_board add constraint pk_board
+primary key (bno);
