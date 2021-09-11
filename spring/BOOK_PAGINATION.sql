7a2a8f5fb1e6503d20505ff5d102e8276ad98f34
CPJ <abzi110@naver.com>
2021-09-12 03:43:15 +0900

    페이징 학습

--- spring/BOOK_PAGINATION.sql
+++ spring/BOOK_PAGINATION.sql
@@ -8,3 +8,19 @@ select /*+INDEX_DESC (tbl_board pk_board) */* from tbl_board;
 
 insert into tbl_board(bno, title, content, writer)
 (select seq_board.nextval, title, content, writer from tbl_board);
+
+-- 페이징
+select /*+INDEX_ASC(tbl_board pk_board)*/ rownum rn, bno, title, content
+from tbl_board
+where rownum <= 120 and rownum < 12;
+
+-- 인라인뷰
+select bno, title, content, writer, regdate, updatedate
+	from(
+		select /*+INDEX_DESC(tbl_board pk_board) */
+			rownum rn, bno, title, content, writer, regdate, updatedate
+		from
+			tbl_board
+		where rownum <= 20
+	)
+	where rn > 10
